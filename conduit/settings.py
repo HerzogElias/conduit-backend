241073531dc28d8965e96f3d19e628ef5991bad7
Elias Herzog <elias.herzog.02@gmail.com>
2025-08-14 23:38:36 +0200

    change static files

--- conduit/settings.py
+++ conduit/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+STATIC_ROOT = "/var/www/127.0.0.1/static/"
 STATIC_URL = '/static/'
 
 CORS_ORIGIN_WHITELIST = (
